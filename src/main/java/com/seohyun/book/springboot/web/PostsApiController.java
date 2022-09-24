71ff3900b91a3892cc44e3cda36c655736540747
seohyun0120 <sarahseohyun@gmail.com>
2022-09-25 01:41:02 +0900

    게시물 수정 / 삭제 기능 추가

--- src/main/java/com/seohyun/book/springboot/web/PostsApiController.java
+++ src/main/java/com/seohyun/book/springboot/web/PostsApiController.java
@@ -1,11 +1,14 @@
 package com.seohyun.book.springboot.web;
 
 import com.seohyun.book.springboot.service.PostsService;
+import com.seohyun.book.springboot.web.dto.PostsListResponseDto;
 import com.seohyun.book.springboot.web.dto.PostsResponseDto;
 import com.seohyun.book.springboot.web.dto.PostsSaveRequestDto;
 import com.seohyun.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -26,5 +29,15 @@ public class PostsApiController {
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id); return id;
+    }
+
+    @GetMapping("/api/v1/posts/list")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
     }
 }
